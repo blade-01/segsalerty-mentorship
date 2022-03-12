8f6d0211379ee17ae54365d8f6bac49e825f3e1b
blade-01 <animashauntaofiq@gmail.com>
2022-03-12 16:46:11 +0100

    Feat: Add icon plugin

---
+++ portfolio/src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" class="rounded-full" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  components: {}
+}
+</script>
