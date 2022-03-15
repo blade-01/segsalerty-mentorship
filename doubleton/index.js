cbf4d20248a7c0ea8b7a309325bf87342a8ba378
blade-01 <animashauntaofiq@gmail.com>
2022-03-15 01:20:42 +0100

    Doc: Update Readme

--- doubleton/index.js
+++
@@ -1,8 +0,0 @@
-function doubleton(n) {
-  const numberSize = new Set(`${++n}`.split("")).size
-  if (numberSize == 2) return n
-  else return doubleton(n)
-}
-
-let answer = doubleton(120)
-console.log(answer)
